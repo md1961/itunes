0d7a2de1bc7d2e4dc51b1cc8e5b7ae76de7b0404
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2021-05-20 13:58:00 +0900

    (tmp) Start on db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+def value_of(key, track)
+  begin
+    track.xpath("key[.='#{key}']").first.next_element.text
+  rescue => e
+    track_id = track.xpath("key[.='Track ID']").first&.next_element&.text
+    STDERR.puts "At Track ID of #{track_id}..."
+    STDERR.puts track
+    raise e
+  end
+end
+
+
+FILENAME = 'db/source/itunes_library.xml'
+
+unless File.exist?(FILENAME)
+  STDERR.puts "Cannot find file '#{FILENAME}'"
+  exit
+end
+
+ApplicationRecord.transaction do
+  Track .destroy_all
+  Album .destroy_all
+  Artist.destroy_all
+end
+
+xml_doc = File.open(FILENAME) { |f| Nokogiri::XML::Document.parse(f) }
+tracks_key = xml_doc   .xpath("/plist/dict/key[.='Tracks']").first
+track_dict = tracks_key.xpath('following-sibling::dict'    ).first
+tracks     = track_dict.xpath('dict')
+
+tracks.each do |track|
+  artist_name = value_of('Artist'     , track)
+  genre       = value_of('Genre'      , track)
+  sort_name   = value_of('Sort Artist', track)
+
+  artist = Artist.find_or_create_by!(name: artist_name)
+  artist.update!(genre: genre, sort_name: sort_name)
+end
