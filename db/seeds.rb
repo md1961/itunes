b4a84013765276adcf960dff75f09c03f5f813e5
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2021-05-20 14:25:59 +0900

    Implement db/seeds.rb for Artist

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,5 @@
 def value_of(key, track)
-  begin
-    track.xpath("key[.='#{key}']").first.next_element.text
-  rescue => e
-    track_id = track.xpath("key[.='Track ID']").first&.next_element&.text
-    STDERR.puts "At Track ID of #{track_id}..."
-    STDERR.puts track
-    raise e
-  end
+  track.xpath("key[.='#{key}']").first&.next_element&.text
 end
 
 
@@ -29,9 +22,11 @@ track_dict = tracks_key.xpath('following-sibling::dict'    ).first
 tracks     = track_dict.xpath('dict')
 
 tracks.each do |track|
-  artist_name = value_of('Artist'     , track)
-  genre       = value_of('Genre'      , track)
-  sort_name   = value_of('Sort Artist', track)
+  artist_name = value_of('Artist', track)
+  next unless artist_name
+
+  genre     = value_of('Genre'      , track)
+  sort_name = value_of('Sort Artist', track)
 
   artist = Artist.find_or_create_by!(name: artist_name)
   artist.update!(genre: genre, sort_name: sort_name)
