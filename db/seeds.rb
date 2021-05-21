3b8b304629b1b21b5bfadad25af3be4a89e144dd
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2021-05-21 09:01:00 +0900

    Refactor db/seeds.rb for upcoming creation of Album's

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,12 @@ def value_of(key, track)
   track.xpath("key[.='#{key}']").first&.next_element&.text
 end
 
+def update_for_non_nil!(model, attr_name, value)
+  return if value.nil?
+  return if model.send(attr_name).present?
+  model.update!("#{attr_name}" => value)
+end
+
 
 FILENAME = 'db/source/itunes_library.xml'
 
@@ -22,15 +28,25 @@ tracks_key = xml_doc   .xpath("/plist/dict/key[.='Tracks']").first
 track_dict = tracks_key.xpath('following-sibling::dict'    ).first
 tracks     = track_dict.xpath('dict')
 
-tracks.each do |track|
+num_tracks = tracks.size
+
+tracks.each_with_index do |track, index|
+  print "  Processing tracks...: #{index + 1}/#{num_tracks}\r"
+
   genre_name = value_of('Genre', track)
   genre = genre_name && Genre.find_or_create_by!(name: genre_name)
 
-  artist_name = value_of('Artist', track)
-  next unless artist_name
-
-  sort_name = value_of('Sort Artist', track)
-
-  artist = Artist.find_or_create_by!(name: artist_name)
-  artist.update!(genre: genre, sort_name: sort_name)
+  artist = value_of('Artist', track).yield_self { |artist_name|
+    if artist_name.nil?
+      nil
+    else
+      sort_name = value_of('Sort Artist', track)
+      Artist.find_or_create_by!(name: artist_name).tap { |artist|
+        update_for_non_nil!(artist, :genre    , genre    )
+        update_for_non_nil!(artist, :sort_name, sort_name)
+      }
+    end
+  }
 end
+
+puts
