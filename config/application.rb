24c604f01ec91ed03f181ec696fb897a0a9f4d51
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2021-06-08 09:24:28 +0900

    Configure rspec for files to generate

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,13 @@ module Itunes
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures:         false,
+        routing_specs:    false,
+        controller_specs: false,
+        view_specs:       false
+    end
   end
 end
