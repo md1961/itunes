f861e63ecacf67a01dd305bdf7b140aa11c649ac
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2021-06-10 07:21:11 +0900

    Refactor db/seeds.rb for variable names of XML elements

--- db/seeds.rb
+++ db/seeds.rb
@@ -37,23 +37,23 @@ end
 
 xml_doc = File.open(FILENAME) { |f| Nokogiri::XML::Document.parse(f) }
 
-tracks_key = xml_doc   .xpath("/plist/dict/key[.='Tracks']").first
-track_dict = tracks_key.xpath('following-sibling::dict'    ).first
-tracks     = track_dict.xpath('dict')
+e_tracks_key = xml_doc   .xpath("/plist/dict/key[.='Tracks']").first
+e_track_dict = e_tracks_key.xpath('following-sibling::dict'    ).first
+e_tracks     = e_track_dict.xpath('dict')
 
-n_tracks = tracks.size
+n_tracks = e_tracks.size
 
-tracks.each_with_index do |track, index|
+e_tracks.each_with_index do |e_track, index|
   print "  Processing tracks...: #{index + 1}/#{n_tracks}\r"
 
-  genre_name = value_of('Genre', track)
+  genre_name = value_of('Genre', e_track)
   genre = genre_name && Genre.find_or_create_by!(name: genre_name)
 
-  artist = value_of('Artist', track).yield_self { |artist_name|
+  artist = value_of('Artist', e_track).yield_self { |artist_name|
     if artist_name.nil?
       nil
     else
-      sort_name = value_of('Sort Artist', track)
+      sort_name = value_of('Sort Artist', e_track)
       Artist.find_or_create_by!(name: artist_name).tap { |artist|
         update_for_non_nil!(artist, :genre    , genre    )
         update_for_non_nil!(artist, :sort_name, sort_name)
@@ -61,14 +61,14 @@ tracks.each_with_index do |track, index|
     end
   }
 
-  album = value_of('Album', track).yield_self { |album_name|
+  album = value_of('Album', e_track).yield_self { |album_name|
     if album_name.nil?
       nil
     else
-      disc_number    = value_of('Disc Number', track)
-      num_discs      = value_of('Disc Count' , track)
-      num_tracks     = value_of('Track Count', track)
-      is_compilation = value_of('Compilation', track)
+      disc_number    = value_of('Disc Number', e_track)
+      num_discs      = value_of('Disc Count' , e_track)
+      num_tracks     = value_of('Track Count', e_track)
+      is_compilation = value_of('Compilation', e_track)
       Album.find_or_create_by!(name: album_name).tap { |album|
         update_for_non_nil!(album, :disc_number   , disc_number   )
         update_for_non_nil!(album, :num_discs     , num_discs     )
@@ -79,15 +79,15 @@ tracks.each_with_index do |track, index|
     end
   }
 
-  track_name = value_of('Name', track)
+  track_name = value_of('Name', e_track)
   next unless track_name
   # TODO: Album Artist:string  ===> Check discrepancy with 'Artist'!!
   Track.create!(
     name:         track_name,
-    id:           value_of('Track ID'    , track),
-    total_time:   value_of('Total Time'  , track),
-    track_number: value_of('Track Number', track),
-    year:         value_of('Year'        , track),
+    id:           value_of('Track ID'    , e_track),
+    total_time:   value_of('Total Time'  , e_track),
+    track_number: value_of('Track Number', e_track),
+    year:         value_of('Year'        , e_track),
     artist:       artist,
     album:        album
   )
@@ -95,34 +95,34 @@ end
 
 puts
 
-playlists_key  = xml_doc       .xpath("/plist/dict/key[.='Playlists']").first
-playlist_array = playlists_key .xpath('following-sibling::array'    ).first
-playlists      = playlist_array.xpath('dict')
+e_playlists_key  = xml_doc       .xpath("/plist/dict/key[.='Playlists']").first
+e_playlist_array = e_playlists_key .xpath('following-sibling::array'    ).first
+e_playlists      = e_playlist_array.xpath('dict')
 
-n_playlists = playlists.size
+n_playlists = e_playlists.size
 
 PLAYLIST_NAMES_TO_SKIP = %w[ダウンロード済み ミュージック ライブラリ]
 
-playlists.each_with_index do |playlist, index|
+e_playlists.each_with_index do |e_playlist, index|
   print "  Processing playlists...: #{index + 1}/#{n_playlists}\r"
 
-  name = value_of('Name', playlist)
+  name = value_of('Name', e_playlist)
   next if PLAYLIST_NAMES_TO_SKIP.include?(name)
 
-  playlist_tracks = playlist.xpath('array/dict')
+  playlist_tracks = e_playlist.xpath('array/dict')
 
   Playlist.create!(
-    id:                   value_of('Playlist ID'           , playlist),
+    id:                   value_of('Playlist ID'           , e_playlist),
     name:                 name,
-    persistent_id:        value_of('Playlist Persistent ID', playlist),
-    parent_persistent_id: value_of('Parent Persistent ID'  , playlist),
-    description:          value_of('Description'           , playlist),
-    is_folder:            value_of('Folder'                , playlist) || false,
-    is_smart_list:        value_of('Smart Info'            , playlist) || false,
-    is_master:            value_of('Master'                , playlist) || false,
-    is_visible:           value_of('Visible'               , playlist) || true ,
-    is_all_items:         value_of('All Items'             , playlist) || false,
-    distinguished_kind:   value_of('Distinguished Kind'    , playlist),
+    persistent_id:        value_of('Playlist Persistent ID', e_playlist),
+    parent_persistent_id: value_of('Parent Persistent ID'  , e_playlist),
+    description:          value_of('Description'           , e_playlist),
+    is_folder:            value_of('Folder'                , e_playlist) || false,
+    is_smart_list:        value_of('Smart Info'            , e_playlist) || false,
+    is_master:            value_of('Master'                , e_playlist) || false,
+    is_visible:           value_of('Visible'               , e_playlist) || true ,
+    is_all_items:         value_of('All Items'             , e_playlist) || false,
+    distinguished_kind:   value_of('Distinguished Kind'    , e_playlist),
   ).tap { |playlist|
     playlist_tracks.each.with_index(1) do |playlist_track, index|
       playlist.playlist_tracks.create!(
