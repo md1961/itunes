ea4a3def8ca43f144327c5274ba58c472b495e8a
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2021-06-10 10:28:43 +0900

    Implement 'albums#index' preliminarily

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'albums/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'albums#index'
+
+  resources :albums, only: %i[index show]
 end
