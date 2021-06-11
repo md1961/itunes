1aaf9ab4dbb8560699df596f93a38a9a65ed0067
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2021-06-11 11:41:51 +0900

    Modify Artist by extending #sort_name and including Comparable

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,6 +1,18 @@
 class Artist < ApplicationRecord
+  include Comparable
+
   belongs_to :genre, optional: true
   has_many :albums
+
+  using StringJpExt
+
+  def sort_name
+    (super || name).downcase.to_hiragana
+  end
+
+  def <=>(other)
+    sort_name <=> other.sort_name
+  end
 
   def to_s
     name
