99b6ac8b5da663818f4073f95543b47a200b4fab
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2021-06-14 15:27:03 +0900

    Change 'albums#index' to display albums artist by artist

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,6 +1,16 @@
 class AlbumsController < ApplicationController
 
   def index
-    @albums = Album.all
+    @artists = Artist.all.sort
+    @albums_by_artist = Album.includes(:artist, :tracks)
+                             .group_by(&:artist)
+                             .map { |artist, albums|
+                               [
+                                 artist,
+                                 albums.sort_by { |album|
+                                   [album.year || 999999]
+                                 }
+                               ]
+                             }.to_h
   end
 end
