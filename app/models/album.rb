69be4551f34fd142f03fc076e202ef1b96dd2454
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2021-06-17 11:43:40 +0900

    Alter Album to include Comparable

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,8 +1,20 @@
 class Album < ApplicationRecord
+  include Comparable
+
   belongs_to :artist, optional: true
   has_many :tracks, -> { order(:track_number) }
 
   def year
     tracks.pluck(:year).compact.max
   end
+
+  def <=>(other)
+    sorter <=> other.sorter
+  end
+
+  protected
+
+    def sorter
+      [year || 999999, name]
+    end
 end
