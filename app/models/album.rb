e84134657abeec69a00f54b8b857c9bb2ab4ba71
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2021-06-17 11:44:22 +0900

    Add scope Album.compilations

--- app/models/album.rb
+++ app/models/album.rb
@@ -4,6 +4,8 @@ class Album < ApplicationRecord
   belongs_to :artist, optional: true
   has_many :tracks, -> { order(:track_number) }
 
+  scope :compilations, -> { where(is_compilation: true) }
+
   def year
     tracks.pluck(:year).compact.max
   end
