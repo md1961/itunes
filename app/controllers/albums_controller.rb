c1f66f68122b8324e05c7170011671c0e80c6209
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2021-06-17 13:44:26 +0900

    Modify 'albums#index' to display compilation albums as well

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,16 +1,13 @@
 class AlbumsController < ApplicationController
 
   def index
-    @artists = Artist.all.sort
+    @artists = [Artist::COMPILATION] + Artist.all.sort
+
     @albums_by_artist = Album.includes(:artist, :tracks)
                              .group_by(&:artist)
                              .map { |artist, albums|
-                               [
-                                 artist,
-                                 albums.sort_by { |album|
-                                   [album.year || 999999]
-                                 }
-                               ]
+                               # Album#artist is nil if it's compilation.
+                               [artist || Artist::COMPILATION, albums.sort]
                              }.to_h
   end
 end
