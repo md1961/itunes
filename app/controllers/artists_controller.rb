21baa1cfd415a3dc805d8a86a2debc8dda80a04a
Naoyuki Kumagai <kumagai@namaka>
2021-10-07 11:29:28 +0900

    Implement 'artists#index' preliminarily

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,4 +1,6 @@
 class ArtistsController < ApplicationController
+
   def index
+    @artists = Artist.all_including_compilation
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'artists/index'
   root 'albums#index'
 
   resources :albums, only: %i[index show]
+
+  resources :artists, only: %i[index show]
 end
