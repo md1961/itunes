6cb802ba7a29e92f75dcbb56157294f991a3c104
Naoyuki Kumagai <kumagai@namaka>
2021-10-13 13:56:46 +0900

    Alter 'artists#index' not to display Artist with no albums

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,6 +2,7 @@ class ArtistsController < ApplicationController
 
   def index
     @artists = Artist.all_including_compilation
+                     .reject { |artist| artist.albums.empty? }
   end
 
   def show
