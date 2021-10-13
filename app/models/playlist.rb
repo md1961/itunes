c9bb0e077c7528f32638f225ed6f28e1e0152b1c
Naoyuki Kumagai <kumagai@namaka>
2021-10-13 14:25:21 +0900

    Correct Playlist by ordering has_many :playlist_tracks by #ordering

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,6 +1,6 @@
 class Playlist < ApplicationRecord
   include BooleanMethodDefiner
 
-  has_many :playlist_tracks, dependent: :destroy
+  has_many :playlist_tracks, -> { order(:ordering) }, dependent: :destroy
   has_many :tracks, through: :playlist_tracks
 end
