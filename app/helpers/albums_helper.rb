722658450e810340cb5d0497e73cdc995fa4518c
Naoyuki Kumagai <kumagai@namaka>
2021-10-14 09:33:35 +0900

    Move helper time_display() in AlbumsHelper to ApplicationHelper

--- app/helpers/albums_helper.rb
+++ app/helpers/albums_helper.rb
@@ -1,8 +1,2 @@
 module AlbumsHelper
-
-  def time_display(time_in_ms)
-    (time_in_ms / 1000.0).divmod(60).yield_self { |min, sec|
-      "#{min}:#{sec.round.to_s.rjust(2, '0')}"
-    }
-  end
 end
