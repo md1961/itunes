e47cde77517b86d3fb6b24dc0cbe492d4308fbe9
Naoyuki Kumagai <kumagai@namaka>
2021-10-14 09:30:15 +0900

    Add scope Playlist.non_folder

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -3,4 +3,6 @@ class Playlist < ApplicationRecord
 
   has_many :playlist_tracks, -> { order(:ordering) }, dependent: :destroy
   has_many :tracks, through: :playlist_tracks
+
+  scope :non_folder, -> { where(is_folder: false) }
 end
