0a82f84727e7972cf6284dc9336ed79a5d999ea6
Naoyuki Kumagai <kumagai@namaka>
2021-11-02 11:01:02 +0900

    Add global helper rating_display()

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 module ApplicationHelper
 
+  def rating_display(ratable)
+    value = ratable.rating || 0
+    num_stars = value / 20
+    '☆' * num_stars
+  end
+
   def time_display(time_in_ms)
     (time_in_ms / 1000.0).divmod(60).yield_self { |min, sec|
       "#{min}:#{sec.round.to_s.rjust(2, '0')}"
