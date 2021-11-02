58911ecfc43b829cf44c76456c23da67f715251d
Naoyuki Kumagai <kumagai@namaka>
2021-11-02 11:00:20 +0900

    Add Track#rating()

--- app/models/track.rb
+++ app/models/track.rb
@@ -2,4 +2,8 @@ class Track < ApplicationRecord
   belongs_to :artist, optional: true
   belongs_to :album , optional: true
   has_one :track_rating
+
+  def rating
+    track_rating&.value
+  end
 end
