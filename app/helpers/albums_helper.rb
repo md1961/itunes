07d4f99e9c2b30a3b4f6e068733118a33cc51a78
Naoyuki Kumagai <kumagai@namaka>
2021-11-09 14:36:30 +0900

    Add helper form_to_put_label_for()

--- app/helpers/albums_helper.rb
+++ app/helpers/albums_helper.rb
@@ -1,2 +1,10 @@
 module AlbumsHelper
+
+  def form_to_put_label_for(album)
+    labels = Albums::Label.all - album.labels
+    form_with url: put_label_album_path(album), local: true, method: :patch do
+      concat select_tag :label_id, options_from_collection_for_select(labels, :id, :name) , prompt: '-'
+      concat submit_tag :label
+    end
+  end
 end
