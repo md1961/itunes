61297bb77c52722e6aec68d13b27e54a744de23e
Naoyuki Kumagai <kumagai@namaka>
2021-11-09 10:48:13 +0900

    Add has_many :album_labels to Albums::Pointer

--- app/models/albums/pointer.rb
+++ app/models/albums/pointer.rb
@@ -1,2 +1,4 @@
 class Albums::Pointer < ApplicationRecord
+  has_many :albums_label_lookups, class_name: 'Albums::LabelLookup', foreign_key: 'albums_pointer_id'
+  has_many :albums_labels, through: :albums_label_lookups
 end
