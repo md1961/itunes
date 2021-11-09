afa26c31be276907ef419de2eb4876628cbffd50
Naoyuki Kumagai <kumagai@namaka>
2021-11-09 09:09:14 +0900

    Generate model Albums::Pointer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_01_231206) do
+ActiveRecord::Schema.define(version: 2021_11_09_000222) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 2021_11_01_231206) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["artist_id"], name: "index_albums_on_artist_id"
+  end
+
+  create_table "albums_pointers", force: :cascade do |t|
+    t.string "artist_name"
+    t.string "album_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "artists", force: :cascade do |t|
