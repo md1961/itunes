d49fe548d29d0a4b3186e4a639ea8de5a838e6ee
Naoyuki Kumagai <kumagai@namaka>
2021-11-09 14:59:09 +0900

    Modify 'albums#show' to display its labels

--- app/helpers/albums_helper.rb
+++ app/helpers/albums_helper.rb
@@ -1,5 +1,13 @@
 module AlbumsHelper
 
+  def labels_display(album)
+    content_tag :div, class: 'labels' do
+      album.labels.pluck(:name).map { |name|
+        concat content_tag :span, name, class: 'label'
+      }
+    end
+  end
+
   def form_to_put_label_for(album)
     labels = Albums::Label.all - album.labels
     form_with url: put_label_album_path(album), local: true, method: :patch do
