3ea1300da2d5e7187426c9423de905d4dacb0116
Naoyuki Kumagai <kumagai@namaka>
2021-11-09 14:39:25 +0900

    Implement 'albums#put_label' with form in 'albums#show'

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -8,4 +8,15 @@ class AlbumsController < ApplicationController
     album = Album.find(params[:id])
     @albums = album.albums_in_set
   end
+
+  def put_label
+    album = Album.find(params[:id])
+    label = Albums::Label.find(params[:label_id])
+    Album.transaction do
+      album.albums_in_set.each do |album|
+        album.put_label(label)
+      end
+    end
+    redirect_to album
+  end
 end
