7b6f134a5c706b736bdb5277cca6ed450e8e78e9
Naoyuki Kumagai <kumagai@namaka>
2021-11-10 10:11:33 +0900

    Implement 'albums#remove_label

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -15,4 +15,11 @@ class AlbumsController < ApplicationController
     album.put_label(label) if label
     redirect_to album
   end
+
+  def remove_label
+    album = Album.find(params[:id])
+    label = Albums::Label.find(params[:label_id])
+    album.remove_label(label)
+    redirect_to album
+  end
 end
