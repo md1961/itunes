7d3e3a25ab837c809f52ca6a2dd3e1a991895fa6
Naoyuki Kumagai <kumagai@namaka>
2021-11-17 14:57:50 +0900

    Add Albums::Label#albums()

--- app/models/albums/label.rb
+++ app/models/albums/label.rb
@@ -1,4 +1,8 @@
 class Albums::Label < ApplicationRecord
   has_many :albums_label_lookups, class_name: 'Albums::LabelLookup', foreign_key: 'albums_label_id'
   has_many :albums_pointers, through: :albums_label_lookups
+
+  def albums
+    albums_pointers.map(&:album)
+  end
 end
