4e9f777666ea539c8d72096314a58ce23fa47650
Naoyuki Kumagai <kumagai@namaka>
2021-11-29 08:52:06 +0900

    Fix by changing helper labels_display() global to be used in 'artists#show'

--- app/helpers/albums_helper.rb
+++ app/helpers/albums_helper.rb
@@ -1,17 +1,5 @@
 module AlbumsHelper
 
-  def labels_display(album)
-    content_tag :div, class: 'labels' do
-      album.labels.map { |label|
-        concat content_tag(:span, class: 'label_with_link') {
-          concat content_tag :span, label.name, class: 'label'
-          concat link_to('x', remove_label_album_path(album, label_id: label),
-                              method: :patch, class: 'link_to_remove_label', hidden: 'hidden')
-        }
-      }
-    end
-  end
-
   def form_to_put_label_for(album)
     labels = Albums::Label.all - album.labels
     form_with url: put_label_album_path(album), local: true, method: :patch do
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,16 @@ module ApplicationHelper
       "#{min}:#{sec.round.to_s.rjust(2, '0')}"
     }
   end
+
+  def labels_display(album)
+    content_tag :div, class: 'labels' do
+      album.labels.map { |label|
+        concat content_tag(:span, class: 'label_with_link') {
+          concat content_tag :span, label.name, class: 'label'
+          concat link_to('x', remove_label_album_path(album, label_id: label),
+                              method: :patch, class: 'link_to_remove_label', hidden: 'hidden')
+        }
+      }
+    end
+  end
 end
