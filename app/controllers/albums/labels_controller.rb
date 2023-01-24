9359ce0288b18bcb5027ea6f5c5fcae35d76caa7
Naoyuki Kumagai <kumagai@namaka>
2023-01-24 14:25:50 +0900

    Implement 'albums/labels#show'

--- app/controllers/albums/labels_controller.rb
+++ app/controllers/albums/labels_controller.rb
@@ -3,4 +3,8 @@ class Albums::LabelsController < ApplicationController
   def index
     @labels = Albums::Label.all_for_action_index
   end
+
+  def show
+    @label = Albums::Label.find(params[:id])
+  end
 end
