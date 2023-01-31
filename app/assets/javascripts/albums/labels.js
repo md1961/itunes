938e88a97b1379896fcb781c7a20fdcfcd488bc9
Naoyuki Kumagai <kumagai@namaka>
2023-01-31 15:05:41 +0900

    Implement 'albums/labels#edit' using Ajax

--- app/assets/javascripts/albums/labels.js
+++ app/assets/javascripts/albums/labels.js
@@ -1,2 +1,22 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+(function() {
+  $(function() {
+
+    if ($('div.albums_labels_edit').length > 0) {
+      const labelId = $('#label_id').data('label-id');
+
+      $('input[name="toggle_label"]').on('change', function(e) {
+        const checkBox = e.target;
+        const albumId = checkBox.dataset.albumId;
+        const action = checkBox.checked ? 'add_album' : 'remove_album';
+        const url = "/albums/labels/" + labelId + "/" + action;
+        $.ajax({
+          type: 'PATCH',
+          url: url,
+          data: {album_id: albumId},
+          success: function(e) {}
+        });
+      });
+    }
+
+  });
+}).call(this);
