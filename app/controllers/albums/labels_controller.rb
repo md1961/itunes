8ac3dcb09cdec4654ab33cc7d3aec1c7877adb51
Naoyuki Kumagai <kumagai@namaka>
2023-01-31 14:59:58 +0900

    Add action and view 'albums/labels#edit' without functionality

--- app/controllers/albums/labels_controller.rb
+++ app/controllers/albums/labels_controller.rb
@@ -1,10 +1,31 @@
 class Albums::LabelsController < ApplicationController
+  before_action :set_label, except: %i[index]
 
   def index
     @labels = Albums::Label.all_for_action_index
   end
 
   def show
-    @label = Albums::Label.find(params[:id])
   end
+
+  def edit
+    @artist_initial_ranges, @initial_range = prepare_artist_initial_ranges
+  end
+
+  def add_album
+    album = Album.find(params[:album_id])
+    album.put_label(@label)
+    render json: {"response": "received"}
+  end
+
+  def remove_album
+    album = Album.find(params[:album_id])
+    album.remove_label(@label)
+  end
+
+  private
+
+    def set_label
+      @label = Albums::Label.find(params[:id])
+    end
 end
