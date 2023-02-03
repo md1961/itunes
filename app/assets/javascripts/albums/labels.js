e5b9c97f932f2f7f194a6d1e28af35a91f85484b
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2023-02-03 10:02:22 +0900

    Modify 'albums/labels#edit' by adding label to each check box

--- app/assets/javascripts/albums/labels.js
+++ app/assets/javascripts/albums/labels.js
@@ -4,7 +4,7 @@
     if ($('div.albums_labels_edit').length > 0) {
       const labelId = $('#label_id').data('label-id');
 
-      $('input[name="toggle_label"]').on('change', function(e) {
+      $('input.toggle_label').on('change', function(e) {
         const checkBox = e.target;
         const albumId = checkBox.dataset.albumId;
         const action = checkBox.checked ? 'add_album' : 'remove_album';
