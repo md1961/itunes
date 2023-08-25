6962d20732d158348d3dfaddda6609ec44fcc2ed
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2023-08-25 13:49:25 +0900

    Alter scope Albums::Label.all_for_action_index to exclude CLASSICAL

--- app/models/albums/label.rb
+++ app/models/albums/label.rb
@@ -2,7 +2,7 @@ class Albums::Label < ApplicationRecord
   has_many :albums_labelings, class_name: 'Albums::Labeling', foreign_key: 'albums_label_id'
   has_many :albums, through: :albums_labelings
 
-  scope :all_for_action_index, -> { where("name NOT IN (?)", %w[CD LP]) }
+  scope :all_for_action_index, -> { where("name NOT IN (?)", %w[CD LP Classical]) }
 
   CLASSICAL = find_by(name: 'Classical')
   def CLASSICAL.albums
